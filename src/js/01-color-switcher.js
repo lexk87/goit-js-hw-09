aebc649ebe05568bd4b673f785720d437b9a69e1
Alexandr Katyshev <katyshev1@gmail.com>
2022-12-13 12:17:08 +0200

    task 1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
-const test = "test";
+const elements = {
+    bodyEl: document.querySelector("body"),
+    startBtnEl: document.querySelector("[data-start]"),
+    stopBtnEl: document.querySelector("[data-stop]"),
+};
+const { bodyEl, startBtnEl, stopBtnEl } = elements;
+let intervalId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+function onStartBtnClick() {
+    intervalId = setInterval(() => {
+        bodyEl.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    startBtnEl.setAttribute("disabled", "disabled");
+};
+
+function onStopBtnClick() {
+    startBtnEl.removeAttribute("disabled");
+    clearInterval(intervalId);
+}
+
+startBtnEl.addEventListener("click", onStartBtnClick);
+stopBtnEl.addEventListener("click", onStopBtnClick);
